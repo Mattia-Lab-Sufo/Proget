d42b2aa4105b25557120f5539059dd952d3610ed
Mattia-Lab-Sufo <mattiaanglano30@gmail.com>
2026-08-30 16:47:00 +0200

    Update Base.lua

--- Base.lua
+++ Base.lua
@@ -1,24 +1,17 @@
 -- File: Main.lua
 
 function setup()
-    -- Scena 3D
     scene = craft.scene()
     
-    -- Telecamera
+    -- Configurazione Telecamera Nativa
     scene.camera.position = vec3(16, 15, -10)
-    scene.camera:get(craft.camera).fov = 60
-    scene.camera:get(craft.camera).rotation = quat.eulerAngles(25, 0, 0)
+    scene.camera.rotation = quat.eulerAngles(25, 0, 0)
     
-    -- Luce
-    local sun = scene:entity()
-    sun:add(craft.light, craft.light.directional)
-    sun.rotation = quat.eulerAngles(45, 45, 0)
-    
-    -- Creazione del Mondo Voxel
+    -- Configurazione Volume Voxel
     worldEntity = scene:entity()
     voxels = worldEntity:add(craft.volume, 32, 32, 32)
     
-    -- Inizializza i blocchi e il terreno
+    -- Inizializzazione blocchi e mappa
     setupBlocks()
     generateTerrain()
     
@@ -33,7 +26,7 @@ function draw()
     update(DeltaTime)
     scene:draw()
     
-    -- Disegna l'interfaccia 2D (Mirino)
+    -- Mirino al centro
     pushStyle()
     stroke(255)
     strokeWidth(2)
