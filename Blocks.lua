74f8fd6c55f8ca3029808b2ba9fde77abd966835
Mattia-Lab-Sufo <mattiaanglano30@gmail.com>
2026-08-30 16:47:28 +0200

    Update Blocks.lua

--- Blocks.lua
+++ Blocks.lua
@@ -1,28 +1,23 @@
 -- File: Blocks.lua
 
 function setupBlocks()
-    -- Registrazione dei blocchi con ID e Colore
     craft.block.register{ id = 1, name = "Grass", color = color(90, 180, 60) }
     craft.block.register{ id = 2, name = "Dirt", color = color(130, 85, 45) }
     craft.block.register{ id = 3, name = "Stone", color = color(120, 120, 120) }
-    craft.block.register{ id = 4, name = "Wood", color = color(100, 60, 30) }
 end
 
 function generateTerrain()
     local scale = 0.08
-    
     for x = 0, 31 do
         for z = 0, 31 do
-            -- Generazione altezza con Perlin Noise
             local h = math.floor(noise(x * scale, z * scale) * 6) + 4
-            
             for y = 0, h do
                 if y == h then
-                    voxels:set(x, y, z, 1) -- Erba
+                    voxels:set(x, y, z, 1)
                 elseif y > h - 3 then
-                    voxels:set(x, y, z, 2) -- Terra
+                    voxels:set(x, y, z, 2)
                 else
-                    voxels:set(x, y, z, 3) -- Pietra
+                    voxels:set(x, y, z, 3)
                 end
             end
         end
